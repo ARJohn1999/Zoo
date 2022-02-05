c227cbd84c85c8909bb0b228344c900c6c2e151e
ARJohn1999 <50611187+ARJohn1999@users.noreply.github.com>
2022-02-05 13:46:24 +0530

    Zebra and Lion added

--- animal.py
+++ animal.py
@@ -1,13 +1,4 @@
 print("Zebra is ready")
 print("Lion is coming")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Yes bugs")
-=======
-print("No bugs")
-print("No bugs")
->>>>>>> 7311e92d6a0a7a1fa74b5e623c1b6faa507ff833
-=======
 
->>>>>>> 1845f64f43c52d33189482ef7556499f1b039c30
 
